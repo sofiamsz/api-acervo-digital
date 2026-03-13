4042114ca5c7be84bc9952d47f4defd026352c5a
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-03-13 20:10:38 -0300

    feat: criando conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
+import { DatabaseModel } from "./model/DatabaseModel.js";
 import { server } from "./server.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 /**
  * Configura a porta do servidor web
  */
-const port: number = 3333;
+const port: number = parseInt(process.env.PORT as string);
+const host: string = process.env.HOST ?? "";
 
 /**
  * Inicia servidor web para escutar requisições
  */
-server.listen(port, () => {
-    console.info(`Servidor executando no endereço http://localhost:${port}`);
-});
+new DatabaseModel().testeConexao().then((ok) => {
+    if (ok) {
+        server.listen(port, () => {
+            console.info(`Servidor executando no endereço ${host}:${port}`);
+        });
+    } else {
+        console.error(`Não foi possível conectar com o banco de dados.`);
+    }
+})
